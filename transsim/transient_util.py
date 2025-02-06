5815ad2f506f9f93da2b9a80baaf8a8ae0486a02
Isaac Sihlangu <isihlangu@sarao.ac.za>
2025-02-06 17:10:42 +0200

    structure change

--- transsim/transient_util.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-class TransientSources:
-    
-    def point_source(self, amplitude, X_rot, Y_rot, sigma_major_pix, sigma_minor_pix):
-        gaussian = amplitude * np.exp(-0.5 * ((X_rot / sigma_major_pix)**2 + (Y_rot / sigma_minor_pix)**2))
-        return gaussian
-
